ddad0d0fd073af6dffd9ccae63b08c5f9eb50b4a
Daniel Chukwudubem <princedaniel7890@gmail.com>
2023-01-26 11:08:51 +0100

    js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const 
+// initialize Dictionary object
+const dictionary = new Dictionary
+
+const letter = document.getElementById('input');
+
+const search = document.getElementById('search')
+
+let timeout;
+
+letter.addEventListener('keyup' , () => {
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+
+        console.log('alert')
+
+    }, 500)
+})
